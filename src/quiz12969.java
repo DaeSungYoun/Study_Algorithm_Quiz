47cb4d1279f7a96221064ff6f8f985d47c4e30fd
DaeSungYoun <ydskingdom@gmail.com>
2022-02-07 22:52:49 +0900

    12969 퀴즈

--- src/quiz12969.java
+++ src/quiz12969.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12969?language=java
+ */
 public class quiz12969 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.println(a + b);
+        print(a, b);
+    }
+
+    public static void print(int n, int m) {
+        StringBuilder stringBuilder;
+
+        for (int i = 0; i < m; i++) {
+            stringBuilder = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                stringBuilder.append('*');
+            }
+            System.out.println(stringBuilder);
+        }
     }
 }
