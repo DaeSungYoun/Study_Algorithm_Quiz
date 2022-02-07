a8bd74d4a68a4584df7f80df050cbeeb057c2824
DaeSungYoun <ydskingdom@gmail.com>
2022-02-07 22:44:26 +0900

    init commit

---
+++ src/quiz12969.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class quiz12969 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+    }
+}
