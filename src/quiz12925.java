0638178c21a3db89ca1824198f297d573ff75718
DaeSungYoun <ydskingdom@gmail.com>
2022-02-12 01:56:37 +0900

    문자열을 정수로 바꾸기

---
+++ src/quiz12925.java
@@ -0,0 +1,10 @@
+public class quiz12925 {
+    public static void main(String[] args) {
+        int solution = solution("123");
+        System.out.println(solution);
+    }
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
