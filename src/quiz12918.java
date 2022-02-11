40d9a00179a12c04133bc195fed0c96b2a90c871
DaeSungYoun <ydskingdom@gmail.com>
2022-02-12 02:41:04 +0900

    문자열 다루기 기본

---
+++ src/quiz12918.java
@@ -0,0 +1,23 @@
+
+public class quiz12918 {
+    public static void main(String[] args) {
+        boolean a234 = solution("12 34");
+        System.out.println(a234);
+    }
+
+    public static boolean solution(String s) {
+        boolean result = true;
+        if (s.length() == 4 || s.length() == 6) {
+            String[] split = s.split("");
+            for (String str : split) {
+                if (!Character.isDigit(str.charAt(0))) {
+                    return false;
+                }
+            }
+        }
+        else{
+            return false;
+        }
+        return result;
+    }
+}
