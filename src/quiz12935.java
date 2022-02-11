38a449614d31d5d10198c098c8ed748ce196fb24
DaeSungYoun <ydskingdom@gmail.com>
2022-02-11 23:31:14 +0900

    제일 작은 수 제거하기

---
+++ src/quiz12935.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class quiz12935 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        int[] solution = solution(arr);
+        System.out.println(Arrays.toString(solution));
+    }
+
+    public static int[] solution(int[] arr) {
+        int asInt = Arrays.stream(arr).min().getAsInt();
+
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int number : arr) {
+            if (number != asInt) {
+                resultList.add(number);
+            }
+        }
+
+        if (resultList.size() == 0) {
+            resultList.add(-1);
+        }
+
+        return resultList.stream().mapToInt(i -> i).toArray();
+    }
+}
